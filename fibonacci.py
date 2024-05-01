8f4db147f219787f3adef14dd4441e24047dc69a
elmauertamas@gmail.com <elmauertamas@gmail.com>
2024-05-01 12:54:25 +0200

    TEST-1 corrected

--- fibonacci.py
+++ fibonacci.py
@@ -1,12 +1,7 @@
 def fibonacci(n):
-    number_fibo = (n+1) * [None]
+    phi = (1 + (5 ** 0.5)) / 2
+    return int((phi**n - (1 - phi)**n) / (5 ** 0.5))
 
-    def recursive_fibonacci(x):
-        if number_fibo[x] is None:
-            number_fibo[x] = x if x <= 1 else recursive_fibonacci(x-1) + recursive_fibonacci(x-2)
-        return number_fibo[x]
+n = 108
 
-    return recursive_fibonacci(n)
-
-for i in range(100):
-    print(i, fibonacci(i))
+print(f"A {n}. Fibonacci-szám: {fibonacci(n)}")
