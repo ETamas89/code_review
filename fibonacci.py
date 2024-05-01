73882584a435cc72516361fe668438bb2e5d551b
ETamas89 <165376924+ETamas89@users.noreply.github.com>
2024-05-01 13:08:07 +0200

    Update fibonacci.py
    Co-authored-by: Dékány Kristóf <gammaray.zedling@gmail.com>

--- fibonacci.py
+++ fibonacci.py
@@ -1,6 +1,8 @@
 def fibonacci(n):
     phi = (1 + (5 ** 0.5)) / 2
-    return int((phi**n - (1 - phi)**n) / (5 ** 0.5))
+    psi = (1 - (5 ** 0.5)) / 2
+    return int((phi**n - psi**n) / (5 ** 0.5))
+
 
 n = 108
 
